4b3bc3e466e591a36951297a291c385e3dd4a0ad
mooomer <sean1.salvador1@gmail.com>
2025-09-16 18:19:53 -0700

    Cleaning up global variables and created initialize population function

--- traveling_salesman.py
+++ traveling_salesman.py
@@ -1,20 +1,31 @@
 from typing import List, Tuple
+from random import shuffle
 
+#Maps points into a tuple
 points3D = Tuple[float, float, float]
 
+#Parse input file into an array
 def parse_input_file(path: str) -> List[points3D]:
-    cities: List[points3D] = []
     with open(path, "r") as f:
         n = int(f.readline().strip())
-        while len(cities) < n:
-            line = f.readline()
-            if not line:
-                break
-            line = line.strip()
-            if not line:
-                continue
-            x, y, z = map(float, line.split())
-            cities.append((x,y,z))
-    return cities
+        return [tuple(map(float, f.readline().split())) for _ in range(n)]
 
-print(parse_input_file('input.txt'))
+#Shuffles the indices of points *population_size* times to represent our initial population
+def initialize_population(num_cities: list, population_size: int) -> List[List[int]]:
+    population = []
+    for i in range(160):
+        tour = num_cities[:]
+        shuffle(tour)
+        population.append(tour)
+    return population
+
+def initialize_matrix(cities):
+    print("placeholder")
+
+def main():
+    cities = parse_input_file("input.txt")
+    initial_list = list(range(len(cities)))
+    population = initialize_population(initial_list, 160)
+    print(population)
+if __name__ == "__main__":
+    main()
